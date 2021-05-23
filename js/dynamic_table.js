e2799b2bf6607f712c8e77f412fca8323162d864
Negoluk Maxim <maksim.negoluk2003@gmail.com>
2021-05-23 19:51:54 +0300

    Додав початки js

--- js/dynamic_table.js
+++ js/dynamic_table.js
@@ -3,25 +3,69 @@ const inProgresColumn = document.querySelector("#inProgres");
 const doneColumn = document.querySelector("#done");
 
 function addTaskToPlanning() {
-	console.log(planningColumn);
 	planningColumn.insertAdjacentHTML(
-    "afterbegin",
-    `<div class="task_block"></div>`
+	"afterbegin",
+	`
+	<div class="task_block">
+		<div class="task_block_top_part">
+			<p class="task_block_title font">To do something</p>
+			<div class="plus delete_button" onclick="deleteTask()"></div>
+		</div>
+		<div class="task_block_bottom_part">
+			<p class="task_block_text font">I have to do something, or not. Ok I will play minecraft anyway</p>
+			<div class="detail_button_block">
+				<div class="details_button">
+				</div>
+			</div>
+		</div>
+	</div>
+	`
    );
 }
 
 function addTaskToInProgres() {
-	console.log(inProgresColumn);
 	inProgresColumn.insertAdjacentHTML(
-    "afterbegin",
-    `<div class="task_block"></div>`
-   );
+	"afterbegin",
+	`
+	<div class="task_block">
+		<div class="task_block_top_part">
+			<p class="task_block_title font">To do something</p>
+			<div class="plus delete_button" onclick="deleteTask()"></div>
+		</div>
+		<div class="task_block_bottom_part">
+			<p class="task_block_text font">I have to do something, or not. Ok I will play minecraft anyway</p>
+			<div class="detail_button_block">
+				<div class="details_button">
+				</div>
+			</div>
+		</div>
+	</div>
+	`
+	);
 }
 
 function addTaskToDone() {
-	console.log(doneColumn);
 	doneColumn.insertAdjacentHTML(
-    "afterbegin",
-    `<div class="task_block"></div>`
+	"afterbegin",
+	`
+	<div class="task_block">
+		<div class="task_block_top_part">
+			<p class="task_block_title font">To do something</p>
+			<div class="plus delete_button" onclick="deleteTask()"></div>
+		</div>
+		<div class="task_block_bottom_part">
+			<p class="task_block_text font">I have to do something, or not. Ok I will play minecraft anyway</p>
+			<div class="detail_button_block">
+				<div class="details_button">
+				</div>
+			</div>
+		</div>
+	</div>
+	`
    );
+}
+
+function deleteTask() {
+	const taskBlock = document.querySelector(".task_block")
+	taskBlock.remove();
 }
